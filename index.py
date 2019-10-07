90cf48a1f7ad1f0c704e41c4e105c657d07b018a
alperensahin <Jarrive33>
2019-10-07 22:02:24 +0300

    init

--- index.py
+++ index.py
@@ -1,23 +1,30 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BOARD)
-control_pins = [7,11,13,15]
+control_pins = [7, 11, 13, 15]
 for pin in control_pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
-halfstep_seq = [
-    [1,0,0,0],
-    [1,1,0,0],
-    [0,1,0,0],
-    [0,1,1,0],
-    [0,0,1,0],
-    [0,0,1,1],
-    [0,0,0,1],
-    [1,0,0,1]
-]
-for i in range(512):
-    for halfstep in range(8):
-        for pin in range(4):
-            GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-        # time.sleep(0.001)
+
+
+def setStep(steps, control_pins=control_pins):
+    for pin in control_pins:
+        GPIO.output(pin, steps[control_pins.index(pin)])
+
+
+def forward(delay, step):
+    for i in range(0, step):
+        setStep([1, 0, 1, 0])
+        time.sleep(delay)
+        setStep([0, 1, 1, 0])
+        time.sleep(delay)
+        setStep([0, 1, 0, 1])
+        time.sleep(delay)
+        setStep([1, 0, 0, 1])
+        time.sleep(delay)
+
+
+
+
 GPIO.cleanup()
